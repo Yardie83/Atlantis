462c6a42f82cf30168118510481563a1a6f8b193
Yardie83 <hermann.grieder@gmail.com>
2016-07-16 22:52:55 +0200

    Deleted everything and started from scratch

---
+++ src/AtlantisServer.java
@@ -0,0 +1,70 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+/**
+ * Created by Hermann Grieder on 16.07.2016.
+ */
+
+public class AtlantisServer {
+
+    private static final int PORT = 9000;
+    private static Socket clientSocket;
+    private static int clientNumber;
+    private static ArrayList<ClientThread> clientThreads = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+
+        AtlantisServer server = new AtlantisServer();
+
+        ServerSocket serverSocket = new ServerSocket(PORT);
+        System.out.println("Server is running");
+        System.out.println("Listening on port: " + PORT);
+
+
+        while (true) {
+            try {
+                System.out.println(clientThreads.size() + " user connected");
+                clientSocket = serverSocket.accept();
+                System.out.println("Connection accepted: " + clientSocket.getInetAddress().getCanonicalHostName());
+                ClientThread chatServerThread = new ClientThread(clientSocket, ++clientNumber, server);
+                clientThreads.add(chatServerThread);
+                chatServerThread.start();
+            } catch (IOException e) {
+                System.out.println("User: " + clientSocket.getInetAddress() + " disconnected");
+            } finally {
+            }
+        }
+    }
+
+    public void removeThread(int clientNumber) {
+        ClientThread chatServerThreadToRemove;
+        boolean found = false;
+        while (!found) {
+            for (ClientThread thread : clientThreads) {
+                if (thread.getClientNumber() == clientNumber) {
+                    chatServerThreadToRemove = thread;
+                    clientThreads.remove(chatServerThreadToRemove.getClientNumber() - 1);
+                    for (ClientThread chatServerThread : clientThreads) {
+                        chatServerThread.sendMessage(String.valueOf("Client #: " + clientNumber + "left"));
+                    }
+                    found = true;
+                }
+            }
+        }
+        if (clientThreads.size() < 2) {
+            System.out.println("Client # " + clientNumber + "disconnected from the server");
+            System.out.println(clientThreads.size() + " client connected");
+        } else {
+            System.out.println(clientThreads.size() + " clients connected");
+        }
+    }
+
+    public ArrayList<ClientThread> getClientThreads() {
+        return clientThreads;
+    }
+}
+
+
+
---
+++ src/ClientThread.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.HashSet;
+
+/**
+ * Created by Hermann Grieder on 16.07.2016.
+ */
+public class ClientThread extends Thread {
+
+    private Socket clientSocket;
+    private int clientNumber;
+    private AtlantisServer server;
+    private BufferedReader inReader;
+    private PrintWriter outWriter;
+    private boolean running = true;
+    private static HashSet<PrintWriter> printWriters = new HashSet<>();
+
+    public ClientThread(Socket clientSocket, int clientNumber, AtlantisServer server) {
+
+        this.clientSocket = clientSocket;
+        this.clientNumber = clientNumber;
+        this.server = server;
+    }
+
+    @Override
+    public void run() {
+        while (running) {
+            try {
+                this.inReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                this.outWriter = new PrintWriter(clientSocket.getOutputStream(), true);
+                printWriters.add(outWriter);
+                System.out.println("PrintWriters: " + printWriters.size());
+                outWriter.println("Welcome to the Server");
+                String fromClientMessage, toClientMessage;
+                while (true) {
+                    fromClientMessage = inReader.readLine();
+                    System.out.println("User " + clientNumber + ": " + fromClientMessage);
+                    toClientMessage = fromClientMessage;
+                    for (PrintWriter outWriter : printWriters) {
+                        outWriter.println(toClientMessage);
+                        System.out.println("Message sent: " + toClientMessage + " to User" + clientSocket.getRemoteSocketAddress());
+                    }
+                    if (fromClientMessage.equals("QUIT")) {
+                        sendGoodBye();
+                        server.removeThread(clientNumber);
+                        running = false;
+                    }
+                }
+            } catch (IOException e) {
+                System.out.println("User #: " + clientNumber + " disconnected \n");
+                server.removeThread(clientNumber);
+                e.printStackTrace();
+                running = false;
+            }
+        }
+    }
+
+    private void sendGoodBye() {
+        outWriter.println("GoodBye");
+    }
+
+    public void sendShutDownMessage() {
+        outWriter.println("Server is shutting down");
+    }
+
+    public int getClientNumber() {
+        return clientNumber;
+    }
+
+    public void sendMessage(String message) {
+        outWriter.println(message);
+    }
+}
